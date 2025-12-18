2ba49e0c10803b38a736723da0c1c33cc6da012c
C202044zxy <16549695@qq.com>
2025-12-18 20:15:07 +0800

    Chore: project setup

---
+++ cs336-basics/cs336_basics/__init__.py
@@ -0,0 +1,10 @@
+from .nn_utils import cross_entropy
+from .optimizer import AdamW
+from .model import BasicsTransformerLM
+
+__all__ = [
+    "cross_entropy",
+    "AdamW",
+    "BasicsTransformerLM",
+]
+
