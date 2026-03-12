47c83ea0c84d7a297b2caabef23b3f53fd90f613
C202044zxy <16549695@qq.com>
2026-03-12 10:51:08 +0800

    Feat: add batch_size dim for flash_attention

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -1,4 +1,7 @@
+import math
+
 import torch
+
 
 class FlashAttention(torch.autograd.Function):
     @staticmethod
@@ -8,39 +11,43 @@ class FlashAttention(torch.autograd.Function):
         K: (batch_size, Nk, d)
         V: (batch_size, Nk, d)
         """
-        print(Q.shape)
-        Nq = Q.shape[0]
-        d = Q.shape[1]
-        Nk = K.shape[0]
+        batch_size, Nq, d = Q.shape
+        Nk = K.shape[1]
         device = Q.device
         dtype = Q.dtype
+        scale = math.sqrt(d)
 
         bq = 16
         bk = 16
         O = torch.zeros_like(Q)
-        L = torch.zeros(Nq, device=device, dtype=dtype)
-        for i in range(0, Nq, bq):
-            q = Q[i: i + bq]   # (bq, d)
-            oi = torch.zeros_like(q)   # (bq, d)
-            li = torch.zeros(q.shape[0], device=device, dtype=dtype)
-            mi = torch.full((q.shape[0],), -float("inf"), device=device, dtype=dtype)
+        L = torch.zeros(batch_size, Nq, device=device, dtype=dtype)
 
-            for j in range(0, Nk, bk):
-                k = K[j: j + bk]
-                v = V[j: j + bk]
+        for b in range(batch_size):
+            for i in range(0, Nq, bq):
+                q = Q[b, i:i + bq]       # (bq, d)
+                cur_bq = q.shape[0]
+                oi = torch.zeros_like(q)  # (bq, d)
+                li = torch.zeros(cur_bq, device=device, dtype=dtype)
+                mi = torch.full((cur_bq,), -float("inf"), device=device, dtype=dtype)
 
-                print(q.shape)
-                print(k.shape)
-                sij = (q @ k.T) / torch.sqrt(d) # (bq, bk)
-                mij = torch.maximum(mi, torch.max(sij, dim = 1).values) # (bq, )
-                pij = torch.exp(sij - mij[:, None]) # (bq, bk)
+                for j in range(0, Nk, bk):
+                    k = K[b, j:j + bk]   # (bk, d)
+                    v = V[b, j:j + bk]   # (bk, d)
 
-                alpha = torch.exp(mi - mij)
-                li = alpha * li + torch.sum(pij, dim = 1)
-                oi = alpha[:, None] * oi + (pij @ v)
+                    sij = (q @ k.T) / scale                          # (bq, bk)
 
-            O[i: i + bq] = oi / li[:, None]
-            L[i: i + bq] = mi + torch.log(li)
+                    mij = torch.maximum(mi, sij.max(dim=1).values)   # (bq,)
+                    pij = torch.exp(sij - mij[:, None])              # (bq, bk)
+
+                    alpha = torch.exp(mi - mij)
+                    li = alpha * li + pij.sum(dim=1)
+                    oi = alpha[:, None] * oi + (pij @ v)
+
+                    mi = mij
+
+                O[b, i:i + bq] = oi / li[:, None]
+                L[b, i:i + bq] = mi + torch.log(li)
+
         ctx.save_for_backward(Q, K, V, L, O)
         return O
     
